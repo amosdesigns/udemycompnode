1c7b0e56de122990a183ad22d5e790417ff0eea4
jerome amos <mail@amosdesigns.net>
2016-08-12 15:55:47 -0400

    added functions.js

---
+++ node-course/functions.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Jerome on 8/12/16.
+ */
+
+function greetUser(name, lastName) {
+    return 'Hello ' + name + ' ' + lastName + "!";
+}
+
+var greeting = greetUser('bob', 'xxxx');
+console.log(greeting);
+//
+function add(a, b) {
+    return a + b;
+}
+console.log(add(3, 6));
+
+var x = add(78, 45);
+console.log(x);
