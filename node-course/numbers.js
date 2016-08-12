2533893ae1cc7af156b648f6b6fdfe0363fa12b6
jerome amos <mail@amosdesigns.net>
2016-08-12 14:42:46 -0400

    added numbers

--- node-course/numbers.js
+++ node-course/numbers.js
@@ -2,4 +2,37 @@
  * Created by Jerome on 8/12/16.
  */
 var age = 25;
+var results;
+results = age +6;
+console.log('age + 6 = '+results);
+
+results = age - 1;
+console.log('age - 1 = '+results);
+
+results = age * 3;
+console.log('age * 3 = '+results);
+
+results = age / 3;
+console.log('age / 3 = '+results);
+
+results = age % 3;
+console.log('age % 3 = '+results);
+
+
+results--;
+console.log('minus 1 : '+results);
+
+
+results++;
+console.log('plus 1 : '+results);
+
+++results;
+console.log('1 plus: '+results);
+
+--results;
+console.log('1 minus:'+results);
+
+var a = 23, b = 30, c  =67, avg;
+avg = (a+b+c)/3;
+console.log('the person average grade is: '+avg);
 
