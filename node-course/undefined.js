e6014ca9f2bb629d40c70ce2d98c594152de4983
jerome amos <mail@amosdesigns.net>
2016-08-13 13:58:12 -0400

    added undefined.js

---
+++ node-course/undefined.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Jerome on 8/13/16.
+ */
+var name = undefined;
+// console.log(name);
+function doesNothing(age) {
+console.log(age);
+}
+// console.log(doesNothing());
+doesNothing();
