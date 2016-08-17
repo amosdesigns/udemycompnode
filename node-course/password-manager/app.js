bae15b897de6b270a6e599d7c5b47c50b1bbf0a8
jerome amos <mail@amosdesigns.net>
2016-08-17 09:43:52 -0400

    updated password manager app

--- node-course/password-manager/app.js
+++ node-course/password-manager/app.js
@@ -8,10 +8,49 @@ console.log('*****************************************************');
 var storage = require('node-persist');
 
 storage.initSync();
+var argv = require('yargs')
+    .command('create', 'creates the new account', function (yargs){
+        "use strict";
+        yargs.options({
+           accountname: {
+                demand: true,
+                alias: 'n',
+                description: "your account name goes here (twitter, facebook)",
+                type: 'string'
+            },
+            username: {
+                demand: true,
+                alias: 'un',
+                description: "your username or email goes here",
+                type: 'string'
+            },
+            password: {
+                demand: true,
+                alias: 'pass',
+                description: "your password goes here",
+                type: 'string'
+            }
+        }).help("help")
+    }).command('get', 'Get an Exiting account', function (yargs) {
+        "use strict";
+        yargs.options({
+            accountname: {
+                demand: true,
+                alias: 'n',
+                description: "your account name goes here (twitter, facebook)",
+                type: 'string'
+            }
+            }).help("help")
+    })
+    .help("help")
+    .argv;
+var command = argv._[0];
+// --accountname facebook
+// --username User12!
+// --password Password123!
 
-// account.name facebook
-// account.username User12!
-// account.password Password123!
+//get
+//  --accountname
 
 function createAccount(account) {
     "use strict";
@@ -37,7 +76,25 @@ function getAccount(accountName) {
     return matchAcc;
 }
 
-var facebookAccount = getAccount('Facebook');
-console.log(facebookAccount);
+if (command ==='create'){
+ var createdAccount = createAccount({
+     name: argv.accountname,
+     username: argv.username,
+     password: argv.password
+});
+    console.log('Account creeated!');
+    console.log(createdAccount);
+} else if (command ==='get'){
+    var gotAccount = getAccount(argv.accountname);
+    if (typeof gotAccount ==="undefined"){
+        console.log('ERROR: Account name Not Found!');
+    } else {
+        console.log('Account name found!');
+        console.log(gotAccount);
+    }
+}
 
-yargs
+
+
+
+
